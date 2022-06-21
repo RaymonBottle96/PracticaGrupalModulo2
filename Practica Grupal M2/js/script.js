820c581f48ea98ac6980377e8ed78aee4c9e880e
RaymonBottle96 <ramon.botella.ciria@gmail.com>
2022-06-21 14:48:43 +0200

    Subida final

--- Practica Grupal M2/js/script.js
+++ Practica Grupal M2/js/script.js
@@ -1,26 +1,17 @@
-function ValidarFormulario(nombre1, apellido1, nombre2, apellido2, telefono, email, direccion, poblacion, provincia, cp, fechaEntrega) {
-    
-    console.log(nombre1);
-    
-    if(nombre1 == "") {
+function ValidarFormulario(nombre, apellidos, telefono, direccion, poblacion, provincia, cp, fechaEntrega) {
+    if(nombre == "") {
         alert("Introduce el nombre");
-    } else if(apellido1 == "") {
+    } else if(apellidos == "") {
         alert("Introduce los apellidos");
-    } else if(nombre2 == "") {
-        alert("Intoduce el nombre de entrega");
-    } else if(apellido2 == "") {
-        alert("Introduce el apellido de entrega");
     } else if(telefono === "") {
         alert("Introduce el telefono");
-    } else if(direccion == "") {
+    } else if(direccion == ""){
         alert("Introduce la direccion");
-    } else if(email == "") {
-        alert("Introduce el email");
     } else if(poblacion == "") {
         alert("Introduce la población");
     } else if(provincia == "") {
         alert("Introduce la provincia");
-    } else if(cp == "") {
+    } else if(cp.isNaN) {
         alert("Introduce el codigo postal"); 
     } else if(fechaEntrega == "") {
         alert("Introduce la fecha de entrega");
@@ -38,8 +29,4 @@ function Funcion1(box) {
     imagenes[3] = "img/rosas_amarillas.jpg";
     imagenes[4] = "img/rosas_blancas.jpg";
     foto.src = imagenes[parseInt(box.value)];
-}
-
-function Funcion2(blabla) {
-    console.log(blabla);
 }
